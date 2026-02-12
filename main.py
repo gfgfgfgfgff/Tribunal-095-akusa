64f91d15338c346f39955339ec03c88a93694d4e
gfgfgfgfgff <leakittydc@gmail.com>
2026-02-12 04:34:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=">", intents=intents)
 
 # ---------- TRIBUNAL VIEW ----------
 class TribunalView(discord.ui.View):
